534e282f643b66ed2ae5d37861621be61c89d598
yangxin1984 <yangxin19841102@163.com>
2017-07-10 14:15:19 +0800

    update: factorial function

--- test.c
+++ test.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
-int main(void *agrv, int agrc)
+int factorial(int x)
 {
-    printf("Hello World!");
+    int ret = 1;
+    
+    if(x <= 0)
+        return 0;
+    
+    while(x) {
+        ret *= x--;
+    }
+    
+    return ret;
+}
+
+int main(int agrc, char *agrv[])
+{
+    if(agrc > 1)
+    {
+        int result = factorial(atoi(agrv[1]));
+        printf("result = %u\n", result);
+    }
+    else
+    {
+        printf("Hello World!\n");
+    }
     
     return 0;
 }
